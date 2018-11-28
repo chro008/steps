7be39d46fa317465dd962206d375dbbaaf8bce15
Leeming <874187203@qq.com>
2018-11-28 16:10:38 +0800

    update

--- js/sz.steps.js
+++ js/sz.steps.js
@@ -1,45 +1,182 @@
-window.onload = function(){
-	var directions = ["horizontal", "vertical"];
-	var lengths = ["short", "long", "normal"];
-	var direction_index = 0;
-	var length_index = 0;
-	var directBtnObj = document.querySelector("#direction-btn");
-	var lengthBtnObj = document.querySelector("#length-btn");
-	var stepsContainer = document.querySelector(".sz-steps");
+(function(global, $){
+	"use strict";
 	
-	directBtnObj.onclick = function(){
-		directBtnObj.value = "change to " + directions[direction_index%2];
-		removeClass(stepsContainer, "sz-steps-" + directions[direction_index%2]);
-		addClass(stepsContainer, "sz-steps-" + directions[(direction_index+1)%2]);
-		direction_index++;
-	};
+	$.prototype.initSteps = function (options) {
+        return new Steps($(this), options);
+    };
 	
-	lengthBtnObj.onclick = function(){
-		lengthBtnObj.value = "change to " + lengths[(length_index+1)%3];
-		removeClass(stepsContainer, "sz-steps-length-" + lengths[(length_index-1)%3]);
-		addClass(stepsContainer, "sz-steps-length-" + lengths[length_index%3]);
-		length_index++;
-	};
-}
+	function Steps(jqobj, options) {
+        var thisobj = this;
+        this.jqobj = jqobj;
+        this.options = $.extend({}, thisobj._defaultOptions, options);
+        this.globalDatas = {
+            current: options.current || 0
+        };
+        if (this._isValid()) {
+            this.init();
+            this.fresh();
+        } else {
+            console.error("分步组件传参错误");
+        }
+    }
 
-function hasClass(elem, cls) {
-  cls = cls || '';
-  if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
-  return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
-}
+    Steps.prototype._defaultOptions = {
+        direction: "vertical",        //horizontal  分步组件的方向
+        connectorSize: "normal",    //long short
+        processDescription: "进行中",
+        waitDescription: "待进行",
+        finishDescription: "已完成",
+        errorDescription: "有错误",
+        stepDetailDescription: [],    //每一步的描述信息
+        maxstep: 5,                     //最多几步
+        minstep: 2,                     //最少几步
+        current: 0,
+        stepcount: 0                    //步骤数总计
+    };
 
-function addClass(ele, cls) {
-  if (!hasClass(ele, cls)) {
-	ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
-  }
-}
- 
-function removeClass(elem, cls) {
-  if (hasClass(elem, cls)) {
-	var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
-	while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-	  newClass = newClass.replace(' ' + cls + ' ', ' ');
-	}
-	elem.className = newClass.replace(/^\s+|\s+$/g, '');
-  }
-}
+    Steps.prototype._isValid = function () {
+        var max = this.options.maxstep;
+        var min = this.options.minstep;
+        var stepcount = this._getStepCount();
+        return stepcount <= max && stepcount >= min;
+    };
+
+    Steps.prototype._getStepCount = function () {
+        var stepcount = this.options.stepcount;
+        if (isNaN(stepcount) || stepcount <= 0) {
+            stepcount = this.options.stepDetailDescription.length || 0;
+        }
+        return stepcount;
+    };
+
+    /**
+     * 初始化
+     */
+    Steps.prototype.init = function () {
+        var direction = this.options.direction;
+        var connectorSize = this.options.connectorSize;
+        if (!direction || (direction !== "vertical" && direction !== "horizontal")) {
+            direction = "horizontal"
+        }
+        if (!connectorSize || (connectorSize !== "normal" && connectorSize !== "long" && connectorSize !== "short")) {
+            connectorSize = "normal"
+        }
+
+        var directionClass = "sz-steps-" + direction;
+        var connectorClass = "sz-steps-length-" + connectorSize;
+        this.jqobj.html("<div class='sz-steps " + directionClass + " " + connectorClass + "'></div>");
+
+        var item_html = "";
+        var count = this._getStepCount();
+        var descriptions = this.options.stepDetailDescription || [];
+        var description;
+        var statusClass;
+        var statusDescription;
+        for (var i = 0; i < count; i++) {
+            //默认第一步是进行中 其他是待进行
+            statusClass = i === 0 ? "sz-steps-status-process" : "sz-steps-status-wait";
+            statusDescription = i === 0 ? this.options.processDescription : this.options.waitDescription;
+            description = descriptions[i] ? descriptions[i] : "";
+            item_html += '<div class="sz-steps-item ' + statusClass + '">' +
+                '<div class="sz-steps-tail">' +
+                '<i></i>' +
+                '</div>' +
+                '<div class="sz-steps-head">' +
+                '<div class="sz-steps-head-inner">' +
+                '<span>' + (i + 1) + '</span>' +
+                '</div>' +
+                '</div>' +
+                '<div class="sz-steps-main">' +
+                '<div class="sz-steps-title">' + statusDescription + '</div>' +
+                '<div class="sz-steps-content">' + description + '</div>' +
+                '</div>' +
+                '</div>';
+        }
+        this.jqobj.find(".sz-steps").html(item_html);
+    };
+
+    /**
+     * 刷新
+     */
+    Steps.prototype.fresh = function () {
+        var current = this.globalDatas.current;
+        var count = this._getStepCount();
+        var classz = "sz-steps-status-process sz-steps-status-wait sz-steps-status-finish sz-steps-status-error";
+        for (var i = 0; i < current; i++) {
+            this.jqobj.find(".sz-steps-item").eq(i).removeClass(classz).addClass("sz-steps-status-finish");
+            this.jqobj.find(".sz-steps-item").eq(i).find(".sz-steps-title").html(this.options.finishDescription);
+        }
+        this.jqobj.find(".sz-steps-item").eq(current).removeClass(classz).addClass("sz-steps-status-process");
+        this.jqobj.find(".sz-steps-item").eq(current).find(".sz-steps-title").html(this.options.processDescription);
+        for (var i = current + 1; i < count; i++) {
+            this.jqobj.find(".sz-steps-item").eq(i).removeClass(classz).addClass("sz-steps-status-wait");
+            this.jqobj.find(".sz-steps-item").eq(i).find(".sz-steps-title").html(this.options.waitDescription);
+        }
+    };
+
+    /**
+     * 下一步
+     */
+    Steps.prototype.next = function () {
+        var count = this._getStepCount();
+        if (this.globalDatas.current < count) {
+            this.globalDatas.current += 1;
+            this.fresh();
+        } else {
+            console.error("next步数超限");
+        }
+    };
+
+    /**
+     * 上一步
+     */
+    Steps.prototype.pre = function () {
+        if (this.globalDatas.current > 0) {
+            this.globalDatas.current -= 1;
+            this.fresh();
+        } else {
+            console.error("pre步数超限");
+        }
+    };
+
+    /**
+     * 跳到step 步骤  step为下标索引  从0开始
+     * @param step
+     */
+    Steps.prototype.jump = function (step) {
+        var count = this._getStepCount();
+        if (!isNaN(step) && step >= 0 && step < count) {
+            this.globalDatas.current = step;
+            this.fresh();
+        } else {
+            console.error("jump步数超限");
+        }
+    };
+
+    /**
+     * 当前步骤提示出错
+     */
+    Steps.prototype.makeError = function () {
+        var current = this.globalDatas.current;
+        var stepItemObj = this.jqobj.find(".sz-steps-item").eq(current);
+        if (stepItemObj) {
+            stepItemObj.addClass("sz-steps-status-error");
+            if (window.sz_steps_errorTimeout) {
+                clearTimeout(window.sz_steps_errorTimeout);
+            }
+            window.sz_steps_errorTimeout = setTimeout(function () {
+                stepItemObj.removeClass("sz-steps-status-error");
+            }, 2000);
+        }
+    };
+
+    /**
+     * 获取当前步骤的小标索引
+     */
+    Steps.prototype.getCurrentStep = function () {
+        return this.globalDatas.current;
+    };
+	
+	!('Steps' in global) && (global.Steps = Steps);
+	
+})(window, $);
